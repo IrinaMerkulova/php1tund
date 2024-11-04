c89c85e2a3c0dabca0e081b8c024c14a3b471714
Irina Merkulova <irina.merkulova@gmail.com>
2024-11-04 11:39:18 +0200

    Mõistatus on vaja lõpuni teha

---
+++ content/moistatus.php
@@ -0,0 +1,9 @@
+<?php
+echo "Mõistatus. Euroopa riik";
+// 6 подсказок при помощи текстовых функций
+// выводить списком <ul> / <ol>
+$riik='';
+echo "<ol>";
+echo "<li>Esimene täht riigis on ".substr($riik,0,1)."</li>";
+echo "</ol>";
+//str_replace()
